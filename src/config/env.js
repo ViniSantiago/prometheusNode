3817c11f5b0172fa779ae7a4b7d70d6cc2dd3b0c
Vinicius Moraes <vinimoraes@bb.com.br>
2018-09-11 09:21:37 -0300

    separacao functions.js, correcao env

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
-export const Env = {
+exports.Env = {
     app: {
         name: process.env.npm_package_name,
         version: process.env.npm_package_version,
@@ -12,7 +12,7 @@ export const Env = {
         port: process.env.API_PORT || 3443,
     },
     kernel: {
-        authorization: process.env.KERNEL_AUTHORIZATION || 'nUgVsgTqYjvNd6EV6Ftmq7ZxYaEkM8Cl',
+        authorization: process.env.KERNEL_AUTHORIZATION || '7egQpg76YVH1YSkdUpzJL8MIbafZ2DJQ6AMFHV6n',
         url_kernel_account: process.env.KERNEL_ACCOUNT || 'https://api.kernel.io.bb.com.br/v0/accounts',
     }
 };
--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,14 +1,11 @@
 "use strict";
-
+require('./functions')
 var Util = require("util");
-var request = require("request");
 var uuid = require("node-uuid");
 var mongoose = require("mongoose");
 var User = mongoose.model("User");
 var Product = mongoose.model("Product");
-var Config = require("../config/env");
 var Prometheus = require("prom-client");
-
 // Prometheus Counter for API calls
 const PrometheusMetrics = {
     requestCounter: new Prometheus.Counter({
@@ -156,73 +153,7 @@ exports.delete_user = function (req, res) {
     });
 };
 
-function newUserValidation(_user) {
-    /* Verifica existencia de mesmo email para outro usuario */
-    return new Promise(function (resolve, reject) {
-        User.findOne({
-            email: _user.email
-        }, null, function (err, user) {
-            if (err) {
-                reject({
-                    error: {
-                        code: 200,
-                        message: "Error validating email - " + err
-                    }
-                });
-            }
-            if (user) {
-                reject({
-                    error: {
-                        code: 1,
-                        message: "Email '" + _user.email + "' already been taken by someone else",
-                        userid: user.userid
-                    }
-                });
-            } else {
-                resolve();
-            }
-        });
-    });
-}
 
-function registerKernel(_user) {
-    var _data = {
-      owner: _user.userid
-    };
-    var _header = {
-      "content-type": "application/json",
-      "x-api-key": Config.Env.kernel.authorization
-    };
-  
-    return new Promise(function (resolve, reject) {
-      request.post({
-          headers: _header,
-          url: Config.Env.kernel.url_kernel_account,
-          body: _data,
-          strictSSL: false,
-          timeout: 1500,
-          json: true
-        },
-        function (erro, res) {
-          console.log("Kernel call statusCode: " + res.statusCode);
-  
-          if (erro || (res.statusCode != 201 )) {
-            console.log("Error reaching Kernel: " + erro);
-            reject({
-              error: {
-                code: 300,
-                message: "Kernel user registration error: " + erro
-              }
-            });
-          } else {
-            console.log("Kernel call resp: " + JSON.stringify(res));
-            console.log("Kernel call OK: " + res.body.data.id);
-            resolve(res.body.data.id);
-          }
-        }
-      );
-    });
-}
 
 exports.list_product = function (req, res) {
     Product.find({}, function (err, product) {
